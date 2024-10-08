a4a1f434a04a58aa3d9c34580080c46400cae42b
Justin <justin.li@optable.co>
2024-10-08 13:55:02 -0400

    address

--- pkg/cmd/cli/io/io.go
+++ pkg/cmd/cli/io/io.go
@@ -12,40 +12,34 @@ func FileReaders(path string) (io.Reader, error) {
 		return os.Stdin, nil
 	}
 
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, fmt.Errorf("os.Open: %w", err)
-	}
-
 	fi, err := os.Stat(path)
 	if err != nil {
 		return nil, fmt.Errorf("os.Stat: %w", err)
 	}
 
-	// regular file
-	if fi.IsDir() {
-		var readers []io.Reader
-		dirEntry, err := os.ReadDir(path)
-		if err != nil {
-			return nil, fmt.Errorf("os.ReadDir: %w", err)
-		}
-
-		for _, entry := range dirEntry {
-			// ignore subdirectories
-			if !entry.IsDir() {
-				f, err := os.Open(filepath.Join(path, entry.Name()))
-				if err != nil {
-					return nil, fmt.Errorf("os.Open: %w", err)
-				}
-
-				readers = append(readers, f)
-			}
-		}
-
-		return io.MultiReader(readers...), nil
+	if !fi.IsDir() {
+		return os.Open(path)
 	}
 
-	return f, nil
+	var readers []io.Reader
+	dirEntry, err := os.ReadDir(path)
+	if err != nil {
+		return nil, fmt.Errorf("os.ReadDir: %w", err)
+	}
+
+	for _, entry := range dirEntry {
+		// ignore subdirectories
+		if !entry.IsDir() {
+			f, err := os.Open(filepath.Join(path, entry.Name()))
+			if err != nil {
+				return nil, fmt.Errorf("os.Open: %w", err)
+			}
+
+			readers = append(readers, f)
+		}
+	}
+
+	return io.MultiReader(readers...), nil
 }
 
 func FileWriter(path string) (io.Writer, error) {
