e734753cab370a56b33c04fe4414b36cf9da440e
Justin <justin.li@optable.co>
2024-10-15 15:45:00 -0400

    use output as match bool and improve Help messages

--- pkg/cmd/cli/run.go
+++ pkg/cmd/cli/run.go
@@ -12,23 +12,24 @@ type (
 	RunCmd struct {
 		PairCleanroomToken string `arg:"" help:"The PAIR clean room token to use for the operation."`
 		AdvertiserKey      string `cmd:"" short:"k" help:"The advertiser private key to use for the operation. If not provided, the key saved in the cofinguration file will be used."`
-		Input              string `cmd:"" short:"i" help:"The input file containing the advertiser data to be hashed and encrypted. If given a directory, all files in the directory will be processed."`
-		Output             string `cmd:"" short:"o" help:"The output file to write the advertiser data to, default to stdout."`
+		Input              string `cmd:"" short:"i" help:"The input file containing the advertiser data to be hashed and encrypted. If a directory path is provided, all files within the directory will be processed."`
 		NumThreads         int    `cmd:"" short:"n" default:"1" help:"The number of threads to use for the operation. Default to 1, and maximum is the number of cores."`
-		Match              bool   `cmd:"" short:"m" help:"Whether to match the two sets of triple encrypted PAIR IDs and output the intersected PAIR IDs."`
+		Output             string `cmd:"" short:"o" help:"The output file to write the intersected PAIR IDs to. If not provided, the intersection will not happen."`
 	}
 )
 
 func (c *RunCmd) Help() string {
 	return `
-This command runs the whole lifecyle of the PAIR clean room operations.
-It first hashes and encrypts the provided advertiser raw identifiers
-using the shared hash salt and advertiser private key,
-and writes the advertiser encrypted identifiers to a secure cloud storage location.
+This command runs the whole lifecycle of the PAIR clean room operations.
+It first hashes and encrypts the provided raw identifiers from the advertiser,
+using the shared hash salt and advertiser's private key,
+and writes the encrypted identifiers to a secure cloud storage location.
 It then reads the publisher's encrypted identifiers,
-and re-encrypts the publisher encrypted identifiers using the same advertiser private key.
-Finally, it matches the two sets of triple encrypted PAIR IDs to then produce the match rate,
-and output the intersected PAIR IDs decrypted with the advertiser private key.
+and re-encrypts the publisher's encrypted identifiers using the same private key from the advertiser.
+Optionally, it matches the two sets of triple-encrypted PAIR IDs to calculate the match rate,
+and outputs the intersected PAIR IDs, decrypted using the advertiser private key.
+Note that the matching operation happens locally on the client side, and should be reserved
+for demo/testing purposes with reasonable data sizes only.
 
 This command can recover from a failure at any step, and will resume from the last successful step.
 `
@@ -81,31 +82,31 @@ func (c *RunCmd) Run(cli *CliContext) error {
 	switch publisherState {
 	case v1.Cleanroom_Participant_DATA_CONTRIBUTED:
 		if advertiserState == v1.Cleanroom_Participant_INVITED {
-			return startFromStepOne(ctx, pairCfg, c.Input, c.Output, c.Match)
+			return startFromStepOne(ctx, pairCfg, c.Input, c.Output)
 		}
 
 		if advertiserState == v1.Cleanroom_Participant_DATA_CONTRIBUTED {
-			return startFromStepTwo(ctx, pairCfg, c.Output, c.Match)
+			return startFromStepTwo(ctx, pairCfg, c.Output)
 		}
 
 		if advertiserState == v1.Cleanroom_Participant_DATA_TRANSFORMED {
-			return startFromStepThree(ctx, pairCfg, c.Output, c.Match)
+			return startFromStepThree(ctx, pairCfg, c.Output)
 		}
 	case v1.Cleanroom_Participant_DATA_TRANSFORMING:
 		fallthrough
 	case v1.Cleanroom_Participant_DATA_TRANSFORMED:
 		if advertiserState == v1.Cleanroom_Participant_DATA_CONTRIBUTED {
-			return startFromStepTwo(ctx, pairCfg, c.Output, c.Match)
+			return startFromStepTwo(ctx, pairCfg, c.Output)
 		}
 
 		if advertiserState == v1.Cleanroom_Participant_DATA_TRANSFORMED {
-			return startFromStepThree(ctx, pairCfg, c.Output, c.Match)
+			return startFromStepThree(ctx, pairCfg, c.Output)
 		}
 	case v1.Cleanroom_Participant_RUNNING:
 		fallthrough
 	case v1.Cleanroom_Participant_SUCCEEDED:
 		if advertiserState == v1.Cleanroom_Participant_DATA_TRANSFORMED {
-			return startFromStepThree(ctx, pairCfg, c.Output, c.Match)
+			return startFromStepThree(ctx, pairCfg, c.Output)
 		}
 	default:
 		return fmt.Errorf("unexpected publisher state: %s", publisherState)
@@ -114,7 +115,7 @@ func (c *RunCmd) Run(cli *CliContext) error {
 	return fmt.Errorf("unexpected advertiser state: %s", advertiserState)
 }
 
-func startFromStepOne(ctx context.Context, pairCfg *pairConfig, input, output string, match bool) error {
+func startFromStepOne(ctx context.Context, pairCfg *pairConfig, input, output string) error {
 	// Step 1 Hash and encrypt the advertiser data and output to advTwicePath.
 	if err := pairCfg.hashEncryt(ctx, input); err != nil {
 		return fmt.Errorf("hashEncryt: %w", err)
@@ -125,7 +126,7 @@ func startFromStepOne(ctx context.Context, pairCfg *pairConfig, input, output st
 		return fmt.Errorf("reEncrypt: %w", err)
 	}
 
-	if !match {
+	if output == "" {
 		return nil
 	}
 
@@ -133,13 +134,13 @@ func startFromStepOne(ctx context.Context, pairCfg *pairConfig, input, output st
 	return pairCfg.match(ctx, output)
 }
 
-func startFromStepTwo(ctx context.Context, pairCfg *pairConfig, output string, match bool) error {
+func startFromStepTwo(ctx context.Context, pairCfg *pairConfig, output string) error {
 	// Step 2. Re-encrypt the publisher's hashed and encrypted PAIR IDs and output to pubTriplePath.
 	if err := pairCfg.reEncrypt(ctx); err != nil {
 		return fmt.Errorf("reEncrypt: %w", err)
 	}
 
-	if !match {
+	if output == "" {
 		return nil
 	}
 
@@ -147,8 +148,8 @@ func startFromStepTwo(ctx context.Context, pairCfg *pairConfig, output string, m
 	return pairCfg.match(ctx, output)
 }
 
-func startFromStepThree(ctx context.Context, pairCfg *pairConfig, output string, match bool) error {
-	if !match {
+func startFromStepThree(ctx context.Context, pairCfg *pairConfig, output string) error {
+	if output == "" {
 		return nil
 	}
 
