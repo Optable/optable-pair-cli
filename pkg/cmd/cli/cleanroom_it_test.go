5efd9da16c41d2354512e16052e775bea9c46cd8
mariiatuzovska <mariia.tuzovska@gmail.com>
2024-12-06 12:08:38 -0500

    Add GetCleanroom tests

--- pkg/cmd/cli/cleanroom_it_test.go
+++ pkg/cmd/cli/cleanroom_it_test.go
@@ -4,7 +4,7 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
-	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,47 +12,103 @@ import (
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
+	v1 "github.com/optable/match-api/v2/gen/optable/external/v1"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestCleanroomRun(t *testing.T) {
 	t.Parallel()
+
+	var (
+		cleanroomName = "cleanrooms/test"
+		salt          = "salt"
+	)
+
+	assertGetRequest := func(r *http.Request) {
+		request, err := io.ReadAll(r.Body)
+		require.NoError(t, err)
+		protoReq := &v1.GetCleanroomRequest{}
+		err = proto.Unmarshal(request, protoReq)
+		require.NoError(t, err)
+		require.Equal(t, cleanroomName, protoReq.Name)
+		require.Equal(t, v1.GetCleanroomRequest_FULL, protoReq.View)
+	}
+
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		switch r.URL.Path {
-		case "/v1/cleanroom/get":
+		case "/admin/api/external/v1/cleanroom/get":
+			assertGetRequest(r)
+
+			cleanroom := v1.Cleanroom{}
+			data, err := proto.Marshal(&cleanroom)
+			if err != nil {
+				panic(err)
+			}
+			_, err = w.Write(data)
+			if err != nil {
+				panic(err)
+			}
 			w.WriteHeader(http.StatusOK)
-			panic("Unexpected call to /v1/cleanroom/get")
-		case "/v1/cleanroom/refresh-token":
-			w.WriteHeader(http.StatusOK)
-			panic("Unexpected call to /v1/cleanroom/refresh-token")
-		case "/v1/cleanroom/advance-advertiser-state":
-			w.WriteHeader(http.StatusOK)
-			panic("Unexpected call to /v1/cleanroom/advance-advertiser-state")
+
 		default:
-			w.WriteHeader(http.StatusOK)
-			panic(fmt.Sprintf("Unexpected call to %s", r.URL.Path))
+			panic("Unexpected call:" + r.URL.Path)
 		}
 	}))
 	defer server.Close()
 
-	cleanroomName := "cleanrooms/test"
-	salt := "salt"
 	token, err := generateToken(server.URL, cleanroomName, salt)
 	require.NoError(t, err)
+
 	getCmd := GetCmd{
 		PairCleanroomToken: token,
 		View:               "full",
 	}
 
-	cli := Cli{
-		CleanroomCmd: CleanroomCmd{Get: getCmd},
-	}
-
+	cli := Cli{CleanroomCmd: CleanroomCmd{Get: getCmd}}
 	cfg := &Config{}
 	cmdCtx, err := cli.NewContext(cfg)
 	require.NoError(t, err)
+
 	err = getCmd.Run(cmdCtx)
 	require.NoError(t, err)
+}
+
+func TestCleanroomRun_RequestFail(t *testing.T) {
+	t.Parallel()
+
+	var (
+		cleanroomName = "cleanrooms/test"
+		salt          = "salt"
+	)
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/admin/api/external/v1/cleanroom/get":
+			w.WriteHeader(http.StatusNotFound)
+
+		default:
+			panic("Unexpected call:" + r.URL.Path)
+		}
+	}))
+	defer server.Close()
+
+	token, err := generateToken(server.URL, cleanroomName, salt)
+	require.NoError(t, err)
+
+	getCmd := GetCmd{
+		PairCleanroomToken: token,
+		View:               "full",
+	}
+
+	cli := Cli{CleanroomCmd: CleanroomCmd{Get: getCmd}}
+	cfg := &Config{}
+	cmdCtx, err := cli.NewContext(cfg)
+	require.NoError(t, err)
+
+	err = getCmd.Run(cmdCtx)
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "404")
 }
 
 func generateToken(url, cleanroomName, salt string) (string, error) {
